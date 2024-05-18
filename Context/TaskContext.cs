7843a9b627dc983bdc19d8dcaa90f7000a97f5eb
dbrandao85 <dbrandao85@hotmail.com>
2024-05-18 11:24:28 -0300

    Add project files.

---
+++ Context/TaskContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeTarefas.Context
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) 
+            : base(options) => Database.EnsureCreated();
+
+        public DbSet<Task> Task { get; set; }
+    }
+}
---
+++ Model/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleDeTarefas.Model
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public DateTime Sla { get; set; }
+        public string ArchiveName { get; set; }
+    }
+}
