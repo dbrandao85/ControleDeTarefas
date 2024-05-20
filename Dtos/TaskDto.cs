d8ec7dd20e6b5d945ac053b676053b9e29832203
dbrandao85 <dbrandao85@hotmail.com>
2024-05-20 06:14:30 -0300

    feat: endpoints funcionando, faltando o upload de arquivo e notificação

--- Dtos/TaskDto.cs
+++ Dtos/TaskDto.cs
@@ -1,14 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ControleDeTarefas.Model
+﻿namespace ControleDeTarefas.Dtos
 {
-    public class Task 
-    { 
-        [Key]
+    public class TaskDto
+    {
         public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Sla { get; set; }
         public string FileName { get; set; }
-        public bool Notified { get; set; }
+        public bool IsPastDue { get; set; }
+        public IFormFile File { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using ControleDeTarefas.Context;
+using ControleDeTarefas.Dtos;
 using Microsoft.EntityFrameworkCore;
-using MySqlX.XDevAPI.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,19 +12,47 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Context>
     (options => options.UseMySQL(connectionString));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost3000", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
+app.UseCors("AllowLocalhost3000");
 
-app.MapPost("AddTask", async (ControleDeTarefas.Model.Task task, Context context) =>
+app.MapPost("AddTask", async (TaskDto taskDto, Context context) =>
 {
+    if (taskDto.File is not null && taskDto.File.Length > 0)
+    {
+        var filePath = Path.Combine("/arquivos", taskDto.File.FileName);
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await taskDto.File.CopyToAsync(stream);
+        }
+    }
+
+    var task = new ControleDeTarefas.Model.Task
+    {
+        Title = taskDto.Title,
+        FileName = taskDto.File?.FileName ?? "",
+        Sla = taskDto.Sla
+    };
+
     context.Task.Add(task);
     await context.SaveChangesAsync();
 
     return Results.Created("Criado com sucesso", task);
 });
 
-app.MapPost("DeleteTask/{id}", async (int id, Context context) =>
+app.MapDelete("DeleteTask/{id}", async (int id, Context context) =>
 {
     var taskToDelete = await context.Task.FirstOrDefaultAsync(task => task.Id == id);
 
@@ -41,12 +69,26 @@ app.MapPost("DeleteTask/{id}", async (int id, Context context) =>
 
 app.MapGet("GetTasks", async (Context context) =>
 {
-    return await context.Task.ToListAsync();
+    var tasks = await context.Task.ToListAsync();
+    var taskDto = new List<TaskDto>();
+
+    foreach (var task in tasks)
+    {
+        taskDto.Add(new TaskDto { 
+            Id = task.Id,
+            Title = task.Title,
+            Sla = task.Sla,
+            FileName = task.FileName,
+            IsPastDue = task.Sla < DateTime.Now });
+    }
+
+    return taskDto;
 });
+
+
 
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.Run();
-
